bdda0a7051d49f45cb3d3e5bbfebfec2047d0608
Andres Ornelas <andres@twitter.com>
2016-08-28 00:03:38 -0700

    Make phone accelerometer rotate planets

---
+++ Assets/PlanetController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetController : MonoBehaviour {
+
+	public Transform referencePoint;
+
+	void Start () {
+		OrientTowardsReference();
+	}
+
+	void Update () {
+		OrientTowardsReference();
+	}
+
+	/**
+	 * Rotate the planet to face the reference point, which makes the planet rotate relative to the camera
+	 */
+	private void OrientTowardsReference() {
+		this.transform.LookAt(referencePoint);
+	}
+}

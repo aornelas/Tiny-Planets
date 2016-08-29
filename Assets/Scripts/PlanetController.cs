33b0902f7a8b7fc0292e20498994d0273ecaaf79
Andres Ornelas <andres@twitter.com>
2016-08-29 14:00:16 -0700

    Add Cardboard Icon to VR button
    And rename project dirs

---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetController : MonoBehaviour {
+
+	public Transform referencePoint;
+	public GameObject nextPlanet;
+	public GameObject vrButton;
+	public float speed = 0.1f;
+
+	void Start() {
+	}
+
+	void OnEnable() {
+		OrientTowardsReference();
+	}
+
+	void Update()
+	{
+		HandleTouch();
+		OrientTowardsReference();
+		if (GvrViewer.Instance.Tilted)
+		{
+			DisableVR();
+		}
+		if (GvrViewer.Instance.Triggered)
+		{
+			NextPlanet();
+		}
+	}
+
+	private void OrientTowardsReference()
+	{
+		this.transform.LookAt(referencePoint);
+	}
+
+	private void DisableVR()
+	{
+		GvrViewer.Instance.VRModeEnabled = false;
+		vrButton.SetActive(true);
+	}
+
+	private void NextPlanet()
+	{
+		this.gameObject.SetActive(false);
+		nextPlanet.SetActive(true);
+	}
+
+	private void HandleTouch()
+	{
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+		{
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			// TODO: Fix me
+			transform.Rotate (-touchDeltaPosition.y * speed, -touchDeltaPosition.x * speed, 0);
+		}
+	}
+}

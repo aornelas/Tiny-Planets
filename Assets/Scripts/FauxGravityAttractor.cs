20bcd1510f81005837b0a7b33247567092652b05
Andres Ornelas <andres@twitter.com>
2016-08-29 18:05:07 -0700

    Add knight character

---
+++ Assets/Scripts/FauxGravityAttractor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityAttractor : MonoBehaviour {
+
+	public float gravity = -10f;
+
+	public void Attract(Transform body) {
+		Vector3 gravityUp = (body.position - transform.position).normalized;
+		Vector3 bodyUp = body.up;
+
+		body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+		Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+		body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityBody : MonoBehaviour {
+
+	public FauxGravityAttractor attractor;
+
+	void Update()
+	{
+		attractor.Attract(transform);
+	}
+}

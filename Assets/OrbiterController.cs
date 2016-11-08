640e93fd63cb99de55808bea94a6afb6a6d5bcc8
Andres Ornelas <andres@twitter.com>
2016-11-08 13:49:16 -0800

    Add Black Hole scene!
    Tiny Planets spin off (haha) where you shoot planets towards a black
    hole to attempt to reach stable orbits.

---
+++ Assets/OrbiterController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbiterController : MonoBehaviour {
+
+	void Update()
+	{
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{		
+		if (other.tag == "Black Hole")
+		{
+			this.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/PlanetSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetSpawner : MonoBehaviour {
+
+	public GameObject camera;
+	public GameObject planetPrefab;
+	public GravityAttractor blackHole;
+
+	void Start()
+	{
+	
+	}
+
+	void Update()
+	{
+		if (GvrViewer.Instance.Triggered)
+		{
+			GameObject planet = (GameObject) Instantiate(planetPrefab, transform.position, new Quaternion());
+			planet.GetComponent<GravityBody>().attractor = blackHole;
+			planet.GetComponent<Rigidbody>().AddForce(camera.transform.forward*100f, ForceMode.VelocityChange);
+		}
+	}
+}

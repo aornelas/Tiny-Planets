13b5d935fa7a50ceea62fe935bc79a0f78a1024b
Andres Ornelas <andres@twitter.com>
2016-11-11 11:31:58 -0800

    Add torque to launched planets

--- Assets/PlanetSpawner.cs
+++ Assets/PlanetSpawner.cs
@@ -8,7 +8,8 @@ public class PlanetSpawner : MonoBehaviour {
 	public GravityAttractor blackHole;
 //	public GravityAttractor blackHole1;
 //	public GravityAttractor blackHole2;
-	public float force = 100f;
+	public float thrust = 100f;
+	public float torque = 25000f;
 
 	void Start()
 	{
@@ -26,7 +27,8 @@ public class PlanetSpawner : MonoBehaviour {
 //			attractors[0] = blackHole1;
 //			attractors[1] = blackHole2;
 //			planet.GetComponent<GravityBody>().attractors = attractors;
-			planet.GetComponent<Rigidbody>().AddForce(camera.transform.forward * force, ForceMode.VelocityChange);
+			planet.GetComponent<Rigidbody>().AddForce(camera.transform.forward * thrust, ForceMode.VelocityChange);
+			planet.GetComponent<Rigidbody>().AddTorque(transform.up * torque);
 		}
 	}
 }

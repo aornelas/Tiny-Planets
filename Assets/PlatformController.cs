51910e04b551c30da5811a682715b61c6db58bee
Andres Ornelas <andres@twitter.com>
2016-11-16 10:14:27 -0800

    Add second ramp to Sphere Maze and make pitch-increasing sounds when ball collides

---
+++ Assets/PlatformController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+
+	public int platformIndex;
+	private float pitchIncrease = 0.1f;
+
+	void OnTriggerEnter(Collider other) 
+	{		
+		if (other.tag == "Ball")
+		{
+			// TODO: Delegate sounds to SphereMazeController so it can play once per platform change
+			GetComponent<AudioSource>().pitch = 1 + (platformIndex * pitchIncrease);
+			GetComponent<AudioSource>().Play();
+		}
+	}
+}
